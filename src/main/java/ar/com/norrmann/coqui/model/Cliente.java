57a55c1b2c9923472e2667cd9be785dd7c084c93
enorrmann <enorrmann@gmail.com>
2015-02-24 13:07:16 -0300

    detalles venta

--- src/main/java/ar/com/norrmann/coqui/model/Cliente.java
+++ src/main/java/ar/com/norrmann/coqui/model/Cliente.java
@@ -1,9 +1,7 @@
 package ar.com.norrmann.coqui.model;
 
 import java.util.List;
-
 import javax.persistence.Query;
-
 import org.springframework.roo.addon.dbre.RooDbManaged;
 import org.springframework.roo.addon.javabean.RooJavaBean;
 import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
@@ -18,12 +16,13 @@ public class Cliente {
     public static List<ar.com.norrmann.coqui.model.Cliente> findAllClientes() {
         return entityManager().createQuery("SELECT o FROM Cliente o order by o.apellido, o.nombres", Cliente.class).getResultList();
     }
-    
+
     public List<ar.com.norrmann.coqui.model.Venta> getVentas() {
-    	Query  query = entityManager().createQuery("SELECT v FROM Venta v where v.cliente = :cliente", Venta.class);
-    	query.setParameter("cliente", this);
+        Query query = entityManager().createQuery("SELECT v FROM Venta v where v.cliente = :cliente", Venta.class);
+        query.setParameter("cliente", this);
         return query.getResultList();
-    }   
+    }
+
     public String getNombreCompleto() {
         return getApellido() + ", " + getNombres();
     }
